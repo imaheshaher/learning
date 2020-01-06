a1c0d1c3d35d079d199603a96e508103a76b2035
imaheshaher <57978715+imaheshaher@users.noreply.github.com>
2020-01-06 20:56:36 +0530

    Update generate_number.py

--- generate_number.py
+++ generate_number.py
@@ -1,13 +1,19 @@
+#generate numbers using given two numbers
+print('generate numbers using given two numbers
+')
+x=int(input('enter the first number'))
+y=int(input('enter the second number'))
+
 k=0
 f=0 
 n=0
-c=0
+
 while k<100:
 	n=a=n
 	while a>0:
 		d=a%10
 		a=int(a/10)
-		if d==3 or d==7:
+		if d==x or d==y:
 			f=1
 		else:
 			f=0
